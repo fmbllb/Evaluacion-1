089754acfb0abe4d3eb3f6295e21095380109e58
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-06-08 16:04:13 -0400

    update model
    fk, y modificacion de la posicion de algunos objetos en el archivo

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -12,27 +12,6 @@ class Usuario(models.Model):
     numero_casa_departamento=models.IntegerField("Numero de Casa")
     direccion=models.CharField("Direccion", max_length=50)
 
-
-class Producto(models.Model):
-    nombre=models.CharField("Nombre Producto", max_length=50)
-    precio=models.IntegerField("Precio")
-    descripcion=models.CharField("Descripcion", max_length=200)
-    categoria_producto=models.CharField("Categoria", max_length=2, choices=TIPO_PRODUCTO)
-
-class Boleta(models.Model):
-    subtotal=models.IntegerField("Subtotal")
-    total=models.IntegerField("Total")
-    fecha_boleta=models.DateField("Fecha", auto_now=False, auto_now_add=False)
-    giro=models.CharField("Giro", max_length=1, choices=TIPO_GIRO)
-    medio_pago=models.CharField("Medio Pago", max_length=2, choices=TIPO_PAGO)
-
-class Factura(models.Model):
-    iva=models.IntegerField("IVA")
-    descripcion=models.CharField("Descripcion", max_length=50)
-    fecha=models.DateField("Fecha", auto_now=False, auto_now_add=False)
-    descuento=models.IntegerField("Descuento")
-    bruto=models.IntegerField("Bruto")
-
 class Promocion(models.Model):
     porcentaje_descuento=models.IntegerField("Descuento")
     descripcion=models.CharField("Descripcion", max_length=50)
@@ -51,6 +30,28 @@ class Tienda(models.Model):
     fecha_creacion=models.DateField("Fecha Creacion", auto_now=False, auto_now_add=False)
     telefono=models.IntegerField("Telefono")
     correo=models.CharField("Correo", max_length=50)
+
+    fk_empleado=models.ForeignKey(Empleado, "ID empleado", on_delete=models.DO_NOTHING)
+
+class Producto(models.Model):
+    nombre=models.CharField("Nombre Producto", max_length=50)
+    precio=models.IntegerField("Precio")
+    descripcion=models.CharField("Descripcion", max_length=200)
+    categoria_producto=models.CharField("Categoria", max_length=2, choices=TIPO_PRODUCTO)
+    foto=models.ImageField(upload_to='personas',null=True)
+
+    fk_tienda=models.ForeignKey(Tienda, "ID Tienda", on_delete=models.DO_NOTHING)
+    fk_promocion=models.ForeignKey(Promocion,"ID Promocion", on_delete=models.DO_NOTHING)
+
+class Boleta(models.Model):
+    subtotal=models.IntegerField("Subtotal")
+    total=models.IntegerField("Total")
+    fecha_boleta=models.DateField("Fecha", auto_now=False, auto_now_add=False)
+    giro=models.CharField("Giro", max_length=1, choices=TIPO_GIRO)
+    medio_pago=models.CharField("Medio Pago", max_length=2, choices=TIPO_PAGO)
+
+    fk_usuario=models.ForeignKey(Usuario, "ID Usuario", on_delete=models.DO_NOTHING)
+    fk_producto=models.ForeignKey(Producto, "ID Producto", on_delete=models.DO_NOTHING)
 
 
 
